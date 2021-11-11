9c9cf03ec8dc5b5ffecd44607cd3e6e3b8731325
ilia <kalinins93@mail.ru>
2021-11-11 15:44:00 +0300

    2

--- src/main/java/org/example/ServingWebContentApplication.java
+++ src/main/java/org/example/ServingWebContentApplication.java
@@ -1,9 +1,13 @@
+package org.example;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ServingWebContentApplication {
+
         public static void main(String[] args) {
-                SpringApplication.run(Application.class, args);
+                SpringApplication.run(ServingWebContentApplication.class, args);
         }
+
 }
