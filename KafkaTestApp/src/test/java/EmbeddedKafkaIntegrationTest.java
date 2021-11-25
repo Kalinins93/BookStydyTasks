f1f2800d8f2b7cbfdf30b847f22e43e8eb3e73cf
ilkalinin <ilkalinin@neoflex.ru>
2021-11-25 14:14:56 +0300

    Add KafkaProject
    Add KafkaProject

---
+++ KafkaTestApp/src/test/java/EmbeddedKafkaIntegrationTest.java
@@ -0,0 +1,39 @@
+import my.kafka.config.KafkaConsumer;
+import my.kafka.config.KafkaProducer;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
+@SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+class EmbeddedKafkaIntegrationTest {
+
+    @Autowired
+    private KafkaConsumer consumer;
+
+    @Autowired
+    private KafkaProducer producer;
+
+    @Value("${test.topic}")
+    private String topic;
+
+    @Test
+    public void givenEmbeddedKafkaBroker_whenSendingtoSimpleProducer_thenMessageReceived()
+            throws Exception {
+        producer.send(topic, "Sending with own simple KafkaProducer");
+        consumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
+
+        assertThat(consumer.getLatch().getCount(), equalTo(0L));
+        assertThat(consumer.getPayload(), containsString("embedded-test-topic"));
+    }
+}
